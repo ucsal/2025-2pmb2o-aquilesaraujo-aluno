0fa77c522f7c59b2634fe8a234650cc5d601fb4d
Aquiles <aquiles2ws@gmail.com>
2025-08-27 16:42:18 -0300

    feat: cria classe para calcular desconto para clientes do tipo 'Premium'

---
+++ src/main/java/br/com/mariojp/solid/ocp/PremiumDiscount.java
@@ -0,0 +1,13 @@
+package br.com.mariojp.solid.ocp;
+
+public class PremiumDiscount implements Discount {
+public CustomerType type;
+	
+	public PremiumDiscount() {
+		this.type = type.PREMIUM;
+	}
+	
+	public double apply(double amount) {
+		return amount * 0.90;
+	}
+}
